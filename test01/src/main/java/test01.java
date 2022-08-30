ddde0eeae9d0cdf1d0dd09fc897d43de5ffe7350
amadeus <weiaili69@gmail.com>
2022-08-30 22:26:27 +0800

    second commit

--- test01/src/main/java/test01.java
+++ test01/src/main/java/test01.java
@@ -2,5 +2,10 @@ public class test01 {
     public static void main(String[] args) {
         String a = "hello world";
         System.out.println(a);
+        System.out.println("");
+    }
+    public void play() {
+        System.out.println("wangle");
+
     }
 }
